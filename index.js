0674551f083784f30f5f415b2bf6313bf907daa1
noureddine ramli <noureddine.ramli2.0@gmail.com>
2026-08-16 15:25:14 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+const btn2 = document.getElementById("submit");
+const resetBtn = document.getElementById("reset");
+let nummer = 0;
+let min = 1;
+let max = 100;
+let counter = 0;
+let text2 = document.getElementById("text");
+let text = document.getElementById("reveal");
+let text3 = document.getElementById("text3");
+let input = document.getElementById("input");
+
+nummer = Math.trunc(Math.random() * max - min) + min;
+console.log(nummer);
+
+btn2.onclick = function () {
+  text.textContent = "";
+  counter += 1;
+  if (input.value == "") {
+    text2.textContent = "you have to write something";
+    counter--;
+  } else if (input.value <= 0) {
+    text2.textContent = "its only numbers between 1 and 100";
+    counter--;
+  } else if (input.value >= 101) {
+    text2.textContent = "its only numbers between 1 and 100";
+    counter--;
+  } else if (input.value > nummer) {
+    text2.textContent = "the number is lower";
+  } else if (input.value < nummer) {
+    text2.textContent = "the number is higher";
+  } else if (input.value == nummer) {
+    text.textContent = `the number was ${nummer}!!`;
+    text2.textContent = "you have got the number!";
+    text3.textContent = `you needed ${counter} tries for that!`;
+  } else {
+    text2.textContent = "you can only do numbers";
+    counter--;
+  }
+  if (counter > 10) {
+    text2.textContent = "pres on restart";
+    text.textContent = `number was ${nummer}`;
+    input.value = "";
+    text3.textContent = `you did more then 10 tries so you lost!!`;
+  }
+};
+console.log(counter);
+reset.onclick = function () {
+  text2.textContent = "";
+  nummer = 0;
+  text.textContent =
+    "   here will be the random number after you have written it you have 10 tries if you did you lost and have to restart!!!";
+  input.value = "";
+  text3.textContent = ``;
+  nummer = Math.trunc(Math.random() * max - min) + min;
+  counter = 0;
+  console.log(nummer);
+};
