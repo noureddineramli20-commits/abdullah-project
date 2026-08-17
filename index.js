0941d5bc54f8eca579e22af598ff05a31ceb99d3
noureddine ramli <noureddine.ramli2.0@gmail.com>
2026-08-17 15:40:21 +0200

    Improve input validation and logging

--- index.js
+++ index.js
@@ -18,32 +18,39 @@ btn2.onclick = function () {
   if (input.value == "") {
     text2.textContent = "you have to write something";
     counter--;
+    console.log(`you have tried ${counter} times`);
   } else if (input.value <= 0) {
     text2.textContent = "its only numbers between 1 and 100";
     counter--;
+    console.log(`you have tried ${counter} times`);
   } else if (input.value >= 101) {
     text2.textContent = "its only numbers between 1 and 100";
     counter--;
+    console.log(`you have tried ${counter} times`);
   } else if (input.value > nummer) {
     text2.textContent = "the number is lower";
+    console.log(`you have tried ${counter} times`);
   } else if (input.value < nummer) {
     text2.textContent = "the number is higher";
+    console.log(`you have tried ${counter} times`);
   } else if (input.value == nummer) {
     text.textContent = `the number was ${nummer}!!`;
     text2.textContent = "you have got the number!";
     text3.textContent = `you needed ${counter} tries for that!`;
+    console.log(`you needed ${counter} tries for that!`);
   } else {
     text2.textContent = "you can only do numbers";
     counter--;
+    console.log(`you have tried ${counter} times`);
   }
   if (counter > 10) {
     text2.textContent = "pres on restart";
     text.textContent = `number was ${nummer}`;
     input.value = "";
     text3.textContent = `you did more then 10 tries so you lost!!`;
+    console.log(`you have tried ${counter} times`);
   }
 };
-console.log(counter);
 reset.onclick = function () {
   text2.textContent = "";
   nummer = 0;
@@ -54,4 +61,5 @@ reset.onclick = function () {
   nummer = Math.trunc(Math.random() * max - min) + min;
   counter = 0;
   console.log(nummer);
+  console.log(`you have tried ${counter} times`);
 };
