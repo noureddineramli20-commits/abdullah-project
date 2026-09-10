702415b01c12e993c4fd2002260fe2e6bdf7bd1e
noureddine ramli <noureddine.ramli2.0@gmail.com>
2026-09-10 20:05:47 +0200

    Refactor singleplayer game logic and favicon change

--- index.js
+++ index.js
@@ -1,106 +1,229 @@
-// Changing Favicon 
+// Changing Favicon
 
 const favicon = document.getElementById("favicon");
 const icons = ["one.png", "two.png", "three.png", "four.png", "five.png"];
 let index = 0;
 
-function changeFavicon()
-{
-	favicon.setAttribute("href", icons[index]);	
-	if(++index > 4)	index = 0;
+function changeFavicon() {
+  favicon.setAttribute("href", icons[index]);
+  if (++index > 4) index = 0;
 }
 
 setInterval(changeFavicon, 120);
 
-// Logic for Game
-const btn = document.getElementById("btn");
-let player = document.getElementById("playerInput");
-let triesText = document.getElementById("triesLeft");
-let tipsText = document.getElementById("lowerOrHigher");
-let min = 1;
-let max = 100;
-let pressed = 0;
-let attemps = 0;
-let running = false;
-let randomNmbr = 0;
-let gameEnds = document.getElementById("gameEnds");
+//this is to escape the intro
+function startintro() {
+  let introDiv = document.getElementById("intro");
+  introDiv.style.display = "none";
+}
+//this is if your choosing to play singleplayer
+function singleplayergamechoice() {
+  let secondPage = document.getElementById("singleOrMultyplayerDiv");
+  let singleplayerpage = document.getElementById("divOfSingleplayer");
+  secondPage.style.display = "none";
+  singleplayerpage.style.display = "flex";
+}
 
-player.style.display = "none";
+//from here is the single player program
+const btnForSingleplayerStart = document.getElementById(
+  "startGameSingleplayer"
+);
+const btnforreplayinmenusingleplayer = document.getElementById(
+  "replaySingleplayerBtn"
+);
+let playerInSingleplayer = document.getElementById("playerInputSingleplayer");
+let triesTextInSingleplayer = document.getElementById("triesLeftSingleplayer");
+let tipsTextInSingleplayer = document.getElementById("lowerOrHigherSingleplayer");
 
-btn.onclick = function () {
-  pressed++;
-  if (!running && pressed === 1) {
-    running = true;
+let triesTextInMenusingleplayer = document.getElementById(
+  "lowerOrHigherInMenu"
+);
+let tipsTextInMenusingleplayer = document.getElementById("triesLeftInMenu");
+let leavemenusingleplayer = document.getElementById("backToMenuSingleplayer");
+let singleplayerpage = document.getElementById("divOfSingleplayer");
 
-    player.value
-      ? window.alert(
-          "you can not write enything if the game didn't began yet!!"
-        )
-      : (player.value = "");
+let minOfSingleplayer = 1;
+let maxOfSingleplayer = 100;
+let pressedInSingleplayer = 0;
+let attempsInSingleplayer = 0;
+let runningInSingleplayer = false;
+let randomNmbrInSingleplayer = 0;
 
-    randomNmbr = Math.trunc(Math.random() * max - min) + min;
+function gamePlaySingleplayer() {
+  pressedInSingleplayer++;
 
-    player.style.display = "block";
+  if (!runningInSingleplayer && pressedInSingleplayer === 1) {
+    runningInSingleplayer = true;
 
-    btn.textContent = "Sumbit";
 
-    player.value = "";
+    randomNmbrInSingleplayer = Math.trunc(Math.random() * maxOfSingleplayer - minOfSingleplayer) + minOfSingleplayer;
 
-    gameEnds.textContent = "";
+    playerInSingleplayer.style.display = "block";
 
-    tipsText.textContent = "The game began";
+    btnForSingleplayerStart.textContent = "Sumbit";
 
-    triesText.textContent = `you have ${10 - attemps} tries left`;
+    playerInSingleplayer.value = "";
+
+    tipsTextInSingleplayer.textContent = "The game began";
+
+    triesTextInSingleplayer.textContent = `you have ${10 - attempsInSingleplayer} tries left`;
   }
-  if (pressed >= 2 && running) {
-    attemps++;
-    btn.textContent = "try again!";
-    if (player.value === "") {
+  if (pressedInSingleplayer >= 2 && runningInSingleplayer) {
+    attempsInSingleplayer++;
+    btnForSingleplayerStart.textContent = "try again!";
+    if (playerInSingleplayer.value === "") {
       window.alert("you have to write something!!");
-      attemps--;
-      player.value = "";
-    } else if (attemps === 10 && player.value == randomNmbr) {
-      tipsText.textContent = `GOOD JOB YOU HAVE GOT THE NUMBER, THE NUMBER WAS INDEED ${randomNmbr}`;
-      triesText.textContent = `YOU HAVE TRIED ${attemps} TIMES`;
-      btn.textContent = "restart";
-      gameEnds.textContent = "THE GAME END";
-      running = false;
-      pressed = 0;
-      attemps = 0;
-      player.style.display = "none";
-      player.value = "";
-    } else if (attemps === 10) {
-      tipsText.textContent = `YOU LOST!! THE NUMBER WAS ${randomNmbr}!`;
-      triesText.textContent = `YOU USED ALL YOUR TRIES`;
-      btn.textContent = "restart";
-      gameEnds.textContent = "THE GAME END";
-      running = false;
-      pressed = 0;
-      attemps = 0;
-      player.style.display = "none";
-      player.value = "";
-    } else if (player.value > max || player.value < min) {
+      attempsInSingleplayer--;
+      playerInSingleplayer.value = "";
+      singleplayerpage.style.backgroundColor = "blue";
+    } else if (attempsInSingleplayer === 10 && playerInSingleplayer.value == randomNmbrInSingleplayer) {
+      tipsTextInMenusingleplayer.textContent = `GOOD JOB YOU HAVE GOT THE NUMBER, THE NUMBER WAS INDEED ${randomNmbrInSingleplayer}`;
+      triesTextInMenusingleplayer.textContent = `YOU HAVE TRIED ${attempsInSingleplayer} TIMES`;
+      btnForSingleplayerStart.style.display = "none";
+      leavemenusingleplayer.style.display = "flex";
+      triesTextInSingleplayer.textContent = "";
+      tipsTextInSingleplayer.textContent = "";
+      let btn2 = document.getElementById("leaveGameSingleplayer");
+      let btn3 = document.getElementById("moreInfoForSingleplayerGame");
+      btn2.style.pointerEvents = "none";
+      singleplayerpage.style.backgroundColor = "lime";
+      btnForSingleplayerStart.textContent = "start";
+      btn3.style.pointerEvents = "none";
+
+      runningInSingleplayer = false;
+      pressedInSingleplayer = 0;
+      attempsInSingleplayer = 0;
+      playerInSingleplayer.style.display = "none";
+      playerInSingleplayer.value = "";
+    } else if (attempsInSingleplayer === 10) {
+      tipsTextInMenusingleplayer.textContent = `YOU LOST!! THE NUMBER WAS ${randomNmbrInSingleplayer}!`;
+      triesTextInMenusingleplayer.textContent = `YOU USED ALL YOUR TRIES`;
+      btnForSingleplayerStart.textContent = "start";
+      btnForSingleplayerStart.style.display = "none";
+      leavemenusingleplayer.style.display = "flex";
+      let btn2 = document.getElementById("leaveGameSingleplayer");
+      let btn3 = document.getElementById("moreInfoForSingleplayerGame");
+      btn2.style.pointerEvents = "none";
+      btn3.style.pointerEvents = "none";
+      singleplayerpage.style.backgroundColor = "red";
+      triesTextInSingleplayer.textContent = "";
+      tipsTextInSingleplayer.textContent = "";
+      runningInSingleplayer = false;
+      pressedInSingleplayer = 0;
+      attempsInSingleplayer = 0;
+      plaplayerInSingleplayeryer.style.display = "none";
+      playerInSingleplayer.value = "";
+    } else if (playerInSingleplayer.value > maxOfSingleplayer || playerInSingleplayer.value < minOfSingleplayer) {
       window.alert("The number is only between 1 and 100");
-      attemps--;
-      player.value = "";
-    } else if (player.value > randomNmbr) {
-      tipsText.textContent = "The random number is lower";
-      player.value = "";
-      triesText.textContent = `you have ${10 - attemps} tries left`;
-    } else if (player.value < randomNmbr) {
-      tipsText.textContent = "The random number is higher";
-      player.value = "";
-      triesText.textContent = `you have ${10 - attemps} tries left`;
-    } else if (player.value == randomNmbr) {
-      tipsText.textContent = `GOOD JOB YOU HAVE GOT THE NUMBER, THE NUMBER WAS INDEED ${randomNmbr}`;
-      triesText.textContent = `YOU HAVE TRIED ${attemps} TIMES`;
-      btn.textContent = "restart";
-      gameEnds.textContent = "THE GAME END";
-      running = false;
-      pressed = 0;
-      attemps = 0;
-      player.style.display = "none";
-      player.value = "";
+      attempsInSingleplayer--;
+      singleplayerpage.style.backgroundColor = "blue";
+      playerInSingleplayer.value = "";
+    } else if (playerInSingleplayer.value > randomNmbrInSingleplayer) {
+      tipsTextInSingleplayer.textContent = "The random number is lower";
+      playerInSingleplayer.value = "";
+      singleplayerpage.style.backgroundColor = "hsl(64, 100%, 70%)";
+      triesTextInSingleplayer.textContent = `you have ${10 - attempsInSingleplayer} tries left`;
+    } else if (playerInSingleplayer.value < randomNmbrInSingleplayer) {
+      tipsTextInSingleplayer.textContent = "The random number is higher";
+      playerInSingleplayer.value = "";
+      singleplayerpage.style.backgroundColor = "hsl(64, 100%, 50%)";
+      triesTextInSingleplayer.textContent = `you have ${10 - attempsInSingleplayer} tries left`;
+    } else if (playerInSingleplayer.value == randomNmbrInSingleplayer) {
+      tipsTextInMenusingleplayer.textContent = `GOOD JOB YOU HAVE GOT THE NUMBER, THE NUMBER WAS INDEED ${randomNmbrInSingleplayer}`;
+      triesTextInMenusingleplayer.textContent = `YOU HAVE TRIED ${attempsInSingleplayer} TIMES`;
+      btnForSingleplayerStart.style.display = "none";
+       btnForSingleplayerStart.textContent = "start";
+      leavemenusingleplayer.style.display = "flex";
+      triesTextInSingleplayer.textContent = "";
+      tipsTextInSingleplayer.textContent = "";
+      let btn2 = document.getElementById("leaveGameSingleplayer");
+      let btn3 = document.getElementById("moreInfoForSingleplayerGame");
+      btn2.style.pointerEvents = "none";
+      btn3.style.pointerEvents = "none";
+      singleplayerpage.style.backgroundColor = "lime";
+      runningInSingleplayer = false;
+      pressedInSingleplayer = 0;
+      attempsInSingleplayer = 0;
+      playerInSingleplayer.style.display = "none";
+      playerInSingleplayer.value = "";
     }
   }
-};
+}
+// to leave the game out of no were in singleplayer
+function leavesingleplayer() {
+  let secondPage = document.getElementById("singleOrMultyplayerDiv");
+  let singleplayerpage = document.getElementById("divOfSingleplayer");
+  secondPage.style.display = "flex";
+  singleplayerpage.style.display = "none";
+  btnForSingleplayerStart.textContent = "start";
+  runningInSingleplayer = false;
+  pressedInSingleplayer = 0;
+  attempsInSingleplayer = 0;
+  playerInSingleplayer.style.display = "none";
+  playerInSingleplayer.value = "";
+  triesTextInSingleplayer.textContent = `you have ${10 - attempsInSingleplayer} tries left`;
+  tipsTextInSingleplayer.textContent = "";
+  singleplayerpage.style.backgroundColor = "hsl(236, 82%, 76%)";
+}
+// to open an info box how to play the game in single player
+function openInfoDiv() {
+  let div = document.getElementById("infoSingleplayerGame");
+  let btn1 = document.getElementById("startGameSingleplayer");
+  let btn2 = document.getElementById("leaveGameSingleplayer");
+  let btn3 = document.getElementById("moreInfoForSingleplayerGame");
+  div.style.display = "flex";
+  btn1.style.pointerEvents = "none";
+  btn2.style.pointerEvents = "none";
+  btn3.style.pointerEvents = "none";
+  playerInSingleplayer.disabled = true;
+
+  btn1.style.opacity = "0.9";
+  btn2.style.opacity = "0.9";
+  btn3.style.opacity = "0.9";
+}
+// to close an info box how to play the game in single player
+function closeInfoDiv() {
+  let div = document.getElementById("infoSingleplayerGame");
+  let btn1 = document.getElementById("startGameSingleplayer");
+  let btn2 = document.getElementById("leaveGameSingleplayer");
+  let btn3 = document.getElementById("moreInfoForSingleplayerGame");
+  div.style.display = "none";
+  btn1.style.pointerEvents = "auto";
+  btn2.style.pointerEvents = "auto";
+  btn3.style.pointerEvents = "auto";
+  playerInSingleplayer.disabled = false;
+
+  btn1.style.opacity = "1";
+  btn2.style.opacity = "1";
+  btn3.style.opacity = "1";
+}
+//this is if the game is done that you can play again. in single player!!
+function restartInMenuSingleplayer() {
+  btnForSingleplayerStart.style.display = "flex";
+  leavemenusingleplayer.style.display = "none";
+  let btn2 = document.getElementById("leaveGameSingleplayer");
+  let btn3 = document.getElementById("moreInfoForSingleplayerGame");
+  btn2.style.pointerEvents = "auto";
+  btn3.style.pointerEvents = "auto";
+  triesTextInSingleplayer.textContent = "you have 10 tries left";
+  tipsTextInSingleplayer.textContent = "";
+  singleplayerpage.style.backgroundColor = "hsl(236, 82%, 76%)";
+}
+//if the game is over and you want to go back to the menu in single player
+function goBackToMenuSingleplayer() {
+  btnForSingleplayerStart.style.display = "flex";
+  leavemenusingleplayer.style.display = "none";
+  let btn2 = document.getElementById("leaveGameSingleplayer");
+  let btn3 = document.getElementById("moreInfoForSingleplayerGame");
+  btn2.style.pointerEvents = "auto";
+  btn3.style.pointerEvents = "auto";
+  triesTextInSingleplayer.textContent = "you have 10 tries left";
+  tipsTextInSingleplayer.textContent = "";
+  let secondPage = document.getElementById("singleOrMultyplayerDiv");
+  let singleplayerpage = document.getElementById("divOfSingleplayer");
+  secondPage.style.display = "flex";
+  singleplayerpage.style.display = "none";
+  singleplayerpage.style.backgroundColor = "hsl(236, 82%, 76%)";
+}
+/////////////////////////////////////////////////////////////
+//this is the 2player game
